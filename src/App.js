7a1be2ba55777afb390c5026512b86f685e2e04e
ErikMyllymaki <erik.myl01@gmail.com>
2022-02-08 21:21:36 +0200

    code to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const URL = "https://api.openweathermap.org/data/2.5/"
+const API_KEY = "3dc7d49c3ac2cbba7fc993b57a40c7c1"
+
 function App() {
+
+  const [city, setCity] = useState("");
+  const [temp, setTemp] = useState(null)
+  const [feels_like, setFeels_like] = useState(null)
+  const [weather, setWeather] = useState(null)
+  const [wind, setWind] = useState(null)
+
+
+  function getWeather() {
+
+    let results = document.getElementById("results")
+    results.style.display = "block"
+    const address = URL + "weather?q=" + city + "&units=metric&lang=fi&appid=" + API_KEY
+
+    axios.get(address)
+      .then((response) => {
+        console.log(response.data)
+        setTemp(response.data.main.temp)
+        setFeels_like(response.data.main.feels_like)
+        setWind(response.data.wind.speed)
+        setWeather(response.data.weather[0].description)
+      }).catch(error => {
+        alert("Kaupungin nimi ei toimi! Kokeile jotain muuta.")
+      })
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hae kaupungin säätiedot</h1>
+      <label>Anna kaupungin nimi:</label>
+      <input type="text" name="city" value={city} onChange={e => setCity(e.target.value)} />
+      <button onClick={getWeather}>Hae säätiedot</button>
+
+      <div id='results'>
+        <p>Lämpötila: {temp} °C (Tuntuu kuin {feels_like} °C)</p>
+        <p>Keli: {weather}</p>
+        <p>Tuulennopeus: {wind} m/s</p>
+      </div>
+
     </div>
   );
 }
